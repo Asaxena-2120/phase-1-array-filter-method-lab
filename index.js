4a5416d39f4675d7320964c43084485ed272a6b2
Aastha <saxena.aastha91@gmail.com>
2023-08-11 14:25:06 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers,name){
+    return drivers.filter((driver)=>driver.toLowerCase()===name.toLowerCase())
+}
+
+function fuzzyMatch(drivers,letters){
+    return drivers.filter((name=>name.slice(0, name.length / 2).includes(letters,0)))
+
+}
+
+function matchName(drivers,name){
+    return drivers.filter((driver=>driver.name===name))
+}
